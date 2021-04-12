169640feba72b8f7d1dce3b1d5bdef1fa42f8344
tomlion777 <48767081+tomlion777@users.noreply.github.com>
2021-04-12 18:58:48 +0100

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,21 @@
+#Int
+a = 10
+print(a)
+
+#Str
+b = "Hello World"
+print(b)
+
+#Float
+c = 0.12453
+print(c)
+
+#Bool
+d = True # or False
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
